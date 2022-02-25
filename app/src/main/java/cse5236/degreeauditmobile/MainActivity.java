014c0b2af12e8bb1a411df15ba96f2a7deaaea16
John Bonds III <johnwb0007@gmail.com>
2022-02-24 20:33:15 -0500

    Created an empty project

---
+++ app/src/main/java/cse5236/degreeauditmobile/MainActivity.java
@@ -0,0 +1,14 @@
+package cse5236.degreeauditmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
